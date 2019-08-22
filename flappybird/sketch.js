e23332e607296e728f8e64e577de52e55956c97c
TomerAmran <50573231+TomerAmran@users.noreply.github.com>
2019-08-22 22:25:30 +0300

    adding collision detection

--- flappybird/sketch.js
+++ flappybird/sketch.js
@@ -1,13 +1,51 @@
 var bird;
 let canvas;
+var blocks = [];
 
 function setup() {
   canvas = createCanvas(300, 500);
   bird = new Bird();
-}
+	blocks.push(new Block());
+ }
+
+
+  function keyPressed(){
+  	if (key == ' ') {
+  		bird.jump();
+  	}
+  }
+
+
 
 function draw() {
-  background(100);
-  bird.update();
-  bird.show();
+  	background(100);
+  	bird.update();
+  	bird.show();
+
+  	if (frameCount % 100 == 0){
+  		blocks.push(new Block());
+  	}
+	for (var i =0 ; i < blocks.length ; i++){
+  		blocks[i].show();
+  		blocks[i].update();
+	}
+	collisionChecker();
+}
+
+
+function collisionChecker() {
+	 //collition between block and bird
+ 	for (var i =0 ; i < blocks.length ; i++){
+  		if (blocks[i].x < (bird.x + bird.size/2) &
+  		 blocks[i].x > (bird.x - bird.size/2 - blocks[i].width) &
+  		  (bird.y <= blocks[i].open - blocks[i].openSize + bird.size/2
+  		   | bird.y >= blocks[i].open + blocks[i].openSize - bird.size/2 ) ) {
+  		noLoop();
+  		}
+	}
+	//bird fall to the ground 
+	if (bird.y > height-10
+		) {
+			noLoop();
+		}
 }
